68c2ae3b0eeea015bfb4351b0881fe3bf554409a
Conall McGinty <conall.mcginty@roli.com>
2025-02-04 11:55:36 +0000

    - Set up some nicer suggestions / default values for the input form - Templated the embedded prompt with the inputs from the form - Saved the output to the clipboard

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
+	"text/template"
+
 	"github.com/charmbracelet/huh"
+	"golang.design/x/clipboard"
 )
 
-func runGitLog(targetBranch, sourceBranch string) (string, error) {
+// embed the prompt file
+//go:embed prompt-template.txt
+var promptTemplate string
+
+
+func getGitPatch(targetBranch, sourceBranch string) (string, error) {
 	targetToSource := fmt.Sprintf("%s..%s", targetBranch, sourceBranch)
 	fmt.Println(targetToSource)
 	// eg: git log -p --full-diff master..RC-001-some-branch
@@ -30,24 +41,89 @@ func isGitRepository() bool {
 	return err == nil
 }
 
+func getGitBranches() ([]string, error) {
+	cmd := exec.Command("git", "branch", "--sort=-committerdate")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, fmt.Errorf("Error getting git branches: %v", err)
+	}
+	lines := strings.Split(string(output), "\n")
+
+	var branches []string
+	for _, line := range lines {
+		trimmed := strings.TrimSpace(line)
+		if trimmed == "" {
+			continue
+		}
+		trimmed = strings.TrimPrefix(trimmed, "* ")
+		branches = append(branches, trimmed)
+	}
+
+	return branches, nil
+
+} 
+
+func getDefaultTargetBranch(branches []string) string {
+	// try to find "master" first
+	for _, branch := range branches {
+		if branch == "master" {
+			return "master"
+		}
+	}
+	// then try to find "main"
+	for _, branch := range branches {
+		if branch == "main" {
+			return "main"
+		}
+	}
+	// if we can't find either, just return an empty string
+	return ""
+}
+
 func main() {
+	// check the current directory is a git repository
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !isGitRepository() {
+		log.Fatal("Current directory: %s is not a git repository", currentDir)
+	}
+
+	// set up variables for the form
 	var (
 		jiraTicket   string
 		sourceBranch string
 		targetBranch string
 	)
 
+	gitBranchSuggestions, _ := getGitBranches()
+	sourceBranch = gitBranchSuggestions[0]
+	targetBranch = getDefaultTargetBranch(gitBranchSuggestions)
+
+	// create the form with some suggestions / default values 
+	// jira ticket: no suggestion or default value cos I cba with the API
+	// source branch:
+	//	default: the most recent branch that was committed to
+	//	suggestions: all branches
+	// target branch:
+	//	default: master, main or nothing. Whichever is found first
+	//	suggestions: all branches
 	form := huh.NewForm(
 		huh.NewGroup(
 			huh.NewInput().
 				Title("What's the JIRA ticket link?").
-				Value(&jiraTicket),
+				Value(&jiraTicket).
+				Placeholder("https://example.atlassian.net/browse/AB-123"),
 			huh.NewInput().
 				Title("What's the source branch?").
-				Value(&sourceBranch),
+				Value(&sourceBranch).
+				Suggestions(gitBranchSuggestions),
 			huh.NewInput().
 				Title("What's the target branch?").
-				Value(&targetBranch),
+				Value(&targetBranch).
+				Suggestions(gitBranchSuggestions),
 		),
 	)
 
@@ -55,23 +131,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	currentDir, err := os.Getwd()
+	// get the patch between the input branches
+	gitPatch, err := getGitPatch(targetBranch, sourceBranch)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("\nJIRA Ticket Link:", jiraTicket)
-	fmt.Println("Source Branch:", sourceBranch)
-	fmt.Println("Target Branch:", targetBranch)
-	fmt.Println("Current Directory:", currentDir)
+	// set up the data for the template
+	data := map[string]string {
+		"TargetBranch": targetBranch,
+		"SourceBranch": sourceBranch,
+		"GitPatch": gitPatch,
+		"JiraTicket": jiraTicket,
+	}
 
-	if !isGitRepository() {
-		log.Fatal("%s is not a git repository", currentDir)
+	t := template.Must(template.New("prompt-output").Parse(promptTemplate))
+
+	// save the templated output into a buffer
+	var outputBytes bytes.Buffer
+	t.Execute(&outputBytes, data)
+
+	// convert buffer to string
+	outputString := outputBytes.String()
+	if outputString == "<nil>" {
+		log.Fatal("Cannot template prompt, you're on your own.")
 	}
-	diffOutput, err := runGitLog(targetBranch, sourceBranch)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(diffOutput)
+
+	fmt.Println("Completed templating prompt, saving to clipboard.")
+
+	// save string to clipboard
+	clipboard.Write(clipboard.FmtText, []byte(outputString))
 
 }
